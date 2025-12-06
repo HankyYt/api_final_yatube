608c39aec702019b5fa2af07b1df0aee1e26fa1f
Timofey <timgurov@yandex.ru>
2025-12-06 22:27:04 +0300

    Revert "изменить пагинацию"
    This reverts commit bd91039abe1dc884a54f53a4b4f60981a5db99b6.

--- yatube_api/api/pagination.py
+++ yatube_api/api/pagination.py
@@ -1,13 +1,24 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 
-class CustomPagination(PageNumberPagination):
-    page_size = 10
+class OptionalLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if ('limit' not in request.query_params
+                and 'offset' not in request.query_params):
+            return None
+        return super().paginate_queryset(queryset, request, view)
 
     def get_paginated_response(self, data):
+        # Если пагинация не применялась, возвращаем просто список
+        if data is None:
+            return Response([])
         return Response({
-            'response': data,
-            'count': self.page.paginator.count,
-            'page_size': self.page_size,
+            'count': self.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
         })
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -99,7 +99,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.OptionalLimitOffsetPagination',
 }
 
 SIMPLE_JWT = {
