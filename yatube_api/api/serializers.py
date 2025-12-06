dfcd7a158d98c69162409f0b30de7e5c16a9ad48
Timofey <timgurov@yandex.ru>
2025-12-06 22:11:57 +0300

    исправить FollowSerializer

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -53,9 +53,9 @@ class FollowSerializer(serializers.ModelSerializer):
             )
         ]
 
-    def validate(self, data):
-        if self.context['request'].user == data['following']:
+    def validate_following(self, value):
+        if self.context['request'].user == value:
             raise serializers.ValidationError(
-                {'following': 'Нельзя подписаться на самого себя!'}
+                'Нельзя подписаться на самого себя!'
             )
-        return data
+        return value
